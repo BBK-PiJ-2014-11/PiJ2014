0f482ec3b56b299326d3e313b023886728f97ac1
unknown <eveera01@CSPC056.dcs.bbk.ac.uk>
2014-10-06 19:58:13 +0100

    latest

---
+++ PATHS.bat
@@ -0,0 +1,13 @@
+set paths.bat
+Regis Machado (rmacha05@mail.bbk.ac.uk)
+Add to contacts
+19:22
+[Keep this message at the top of your inbox]
+To: ehshan@msn.com
+rmacha05@mail.bbk.ac.uk
+h:
+PATH=%PATH%;I:\mytools\JDecafCompiler-master\
+echo javadecaf has been set
+PATH=%PATH%;C:\Program Files (x86)\Git\bin\
+echo javadecaf has been set
+pause > nul
